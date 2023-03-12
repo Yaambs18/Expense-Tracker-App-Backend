d7b67ea4ac4becfc18b955549320b6eccece9e4d
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-12 22:14:25 +0530

    Implemented bcrypt for password encryption

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 function isStringInvalid(string){
     if(string == undefined || string.length === 0){
@@ -17,34 +18,49 @@ exports.addUser = async (req, res, next) => {
             return res.status(400).json({err : 'Bad Parameters: Something is missing'});
         }
 
+        const passHash = await bcrypt.hash(password, 10);
         await User.create({
             name: name,
             email: email,
-            password: password
+            password: passHash
         })
         res.status(201).json({message: 'Successfully created a new user'});
     }
     catch(err) {
         console.log(err);
-        res.sendStatus(505).json(err);        
+        res.sendStatus(500).json(err);        
     }
 }
 
 exports.loginUser = async (req, res, next) =>{
     try {
-        const user = await User.findAll({where : {email: req.body.email}});
-        if(user[0] == null){
-            res.status(404).json({message: "User doesn't exist. Please Sign Up !!"});
+        const { email, password } = req.body;
+
+        if(isStringInvalid(email) || isStringInvalid(password)){
+            return res.status(400).json({err : 'Bad Parameters: Something is missing'});
         }
-        else if(user[0].password !== req.body.password){
-            res.status(401).json({message: "Email or Password is Incorrect. Please try again !!!"});
+
+        const user = await User.findAll({where : {email: req.body.email}});
+        if(user.length > 0){
+            bcrypt.compare(password, user[0].password, (err, result) => {
+                if(err){
+                    console.log(error);
+                    res.sendStatus(500).json(error);
+                }
+                if(result){
+                    res.json({message: 'User Login Successful'});
+                }
+                else{
+                    res.status(401).json({message: "Password is Incorrect. Please try again !!!"});
+                }
+            });
         }
         else{
-            res.json({message: 'User Login Successful'});
+            res.status(404).json({message: "User doesn't exist. Please Sign Up !!"});
         }
     }
     catch(error) {
         console.log(error);
-        res.sendStatus(505).json(error);
+        res.sendStatus(500).json(error);
     }
 }
