314c6b16d6114a695fc82c3bae97826248aab78c
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-16 13:01:38 +0530

    added dowload expense report feature for premium users

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
-const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const AWS = require('aws-sdk');
+
+const User = require('../models/user');
+const Expense = require('../models/expense');
 
 function isStringInvalid(string){
     if(string == undefined || string.length === 0){
@@ -9,6 +12,39 @@ function isStringInvalid(string){
     else{
         return false;
     }
+}
+
+function uploadToS3(data, fileName){
+    const BUCKET_NAME = process.env.BUCKET_NAME;
+    const IAM_USER_KEY = process.env.IAM_USER_KEY;
+    const IAM_USER_SECRET = process.env.IAM_USER_SECRET;
+
+    let s3bucket = new AWS.S3({
+        accessKeyId: IAM_USER_KEY,
+        secretAccessKey: IAM_USER_SECRET
+    });
+
+    
+    var params = {
+        Bucket: BUCKET_NAME,
+        Key: fileName,
+        Body: data,
+        ACL: 'public-read'
+    };
+
+    return new Promise((resolve, reject) => {
+        s3bucket.upload(params, (err, s3response) => {
+            if(err){
+                console.log('Something went wrong', err);
+                reject(err);
+            }else{
+                // console.log('Success', s3response);
+                resolve(s3response.Location);
+            }
+        });
+    });
+
+
 }
 
 const generateToken = async function (id){
@@ -73,8 +109,29 @@ const loginUser = async (req, res, next) =>{
     }
 }
 
+const downloadExpenseReport = async (req, res, next) => {
+    try {
+        const reqUser = req.user;
+        if(!reqUser.ispremiumuser){
+            res.status(401).json({ success: false, messasge: 'Unauthorised user'});
+        }
+        const userExpenses = await Expense.findAll({ where: {userId: reqUser.id }});
+        
+        // console.log(userExpenses);
+        const stringifiedExpenses = JSON.stringify(userExpenses);
+        const filename = `Expense${reqUser.id}/${new Date()}`;
+        const fileUrl = await uploadToS3(stringifiedExpenses, filename);
+        res.status(201).json({fileUrl, success: true});
+    }
+    catch(error) {
+        console.log(error);
+        res.sendStatus(500).json(error); 
+    }
+}
+
 module.exports = {
     addUser,
     loginUser,
-    generateToken
+    generateToken,
+    downloadExpenseReport
 }
